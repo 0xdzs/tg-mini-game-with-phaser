ca080a59460fff1df10bee25e658a5d6c77305b2
0xdzs <devon@trooperapp.cn>
2024-11-07 01:21:54 +0800

    changed progress bar size and position

--- src/scenes/Preloader.js
+++ src/scenes/Preloader.js
@@ -13,17 +13,15 @@ export class Preloader extends Scene
         this.add.image(512, 384, 'preloader');
 
         //  A simple progress bar. This is the outline of the bar.
-        this.add.rectangle(512, 384, 468, 32).setStrokeStyle(1, 0xffffff);
+        this.add.rectangle(212, 442, 300, 32).setStrokeStyle(1, 0xffffff);
 
         //  This is the progress bar itself. It will increase in size from the left based on the % of progress.
-        const bar = this.add.rectangle(512-230, 384, 4, 28, 0xffffff);
+        const bar = this.add.rectangle(212-146, 442, 4, 28, 0xffffff);
 
         //  Use the 'progress' event emitted by the LoaderPlugin to update the loading bar
         this.load.on('progress', (progress) => {
-
-            //  Update the progress bar (our bar is 464px wide, so 100% = 464px)
-            bar.width = 4 + (460 * progress);
-
+            //  Update the progress bar (our bar is 296px wide, so 100% = 296px)
+            bar.width = 4 + (292 * progress);
         });
     }
 
